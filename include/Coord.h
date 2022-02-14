3294d8373c6883d92d3370db9de5a102367ac2b7
Shane McDonough <shanemcdman@gmail.com>
2022-02-14 11:18:38 -0800

    Create Coord

---
+++ include/Coord.h
@@ -0,0 +1,12 @@
+#ifndef COORD_H
+#define COORD_H
+
+typedef struct{
+    int x;
+    int y;
+} Coord;
+
+Coord new_coord(int x, int y);
+
+#endif
+
---
+++ src/Coord.c
@@ -0,0 +1,8 @@
+#include"Coord.h"
+
+Coord new_coord(int x, int y){
+    Coord c;
+    c.x = x;
+    c.y = y;
+    return c;
+}
