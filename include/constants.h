7d69d323f46a69ecf76a2ec9c07e5961ecc568f8
Shane McDonough <shanemcdman@gmail.com>
2022-02-15 09:51:54 -0800

    Add basic movement

--- include/constants.h
+++ include/constants.h
@@ -1,9 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define BOARD_SIZE 20
+#define BOARD_SIZE 40
 
 // piece representations
+#define R_EMPTY "  "
 #define R_HEAD "\033[42m  "
 #define R_TAIL "\033[102m\033[36mXX"
 #define R_FRUIT "\033[31m▝▘"
--- src/snake.c
+++ src/snake.c
@@ -11,6 +11,8 @@ typedef enum{
     RIGHT
 } Direction;
 
+#define DIRECTION_BUFFER_SIZE 4
+
 Deque snake;
 int length_to_add;
 bool running;
@@ -22,19 +24,70 @@ void kbin(){
         case EOF: // no input
             return;
         case 'w':
-            current_direction = UP;
+            if(current_direction != DOWN){
+                current_direction = UP;
+            }
             break;
         case 'a':
-            current_direction = LEFT;
+            if(current_direction != RIGHT){
+                current_direction = LEFT;
+            }
             break;
         case 's':
-            current_direction = DOWN;
+            if(current_direction != UP){
+                current_direction = DOWN;
+            }
             break;
         case 'd':
-            current_direction = RIGHT;
+            if(current_direction != LEFT){
+                current_direction = RIGHT;
+            }
             break;
     }
-    printf("%d", current_direction);
+}
+
+void draw(){
+    put_block(snake.head->val, R_HEAD);
+    if(snake.head->next != NULL){
+        put_block(snake.head->next->val, R_TAIL);
+    }
+}
+
+void erase_end_of_tail(){
+    put_block(snake.tail->val, R_EMPTY);
+}
+
+void update(){
+    Coord new_head = new_coord(snake.head->val.x, snake.head->val.y);
+    switch(current_direction){
+        case UP:
+            new_head.y -= 1;
+            break;
+        case DOWN:
+            new_head.y += 1;
+            break;
+        case LEFT:
+            new_head.x -= 1;
+            break;
+        case RIGHT:
+            new_head.x += 1;
+            break;
+    }
+    if(
+        new_head.x <= 0 ||
+        new_head.y <= 0 ||
+        new_head.x > BOARD_SIZE ||
+        new_head.y > BOARD_SIZE
+    ){
+        running = false;
+        return;
+    }
+    deque_push_front(&snake, new_head);
+    if(length_to_add > 0){
+        length_to_add--;
+    }else{
+        put_block(deque_pop_back(&snake), R_EMPTY);
+    }
 }
 
 void start(){
@@ -45,6 +98,8 @@ void start(){
     current_direction = UP;
     while(running){
         kbin();
+        update();
+        draw();
         refresh();
         usleep(50000); // wait 0.05 seconds
     }
